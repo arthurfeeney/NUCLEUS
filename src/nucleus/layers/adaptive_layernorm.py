fix precision changes

src/nucleus/layers/adaptive_layernorm.py
@@ -2,10 +2,15 @@ import torch
 import torch.nn as nn
 
 class AdaptiveLayerNorm(nn.Module):
-    def __init__(self, embed_dim: int, num_sim_params: int, eps: float = 1e-6):
+    def __init__(self, embed_dim: int, num_sim_params: int, dtype: torch.dtype, eps: float = 1e-6):
         super().__init__()
         self.embed_dim = embed_dim
         self.norm = nn.LayerNorm(embed_dim, eps=eps, elementwise_affine=False)
+        
+        # modulation is applied in dtype
+        self.dtype = dtype
+        
+        # Modulation is computed as float32
         self.modulation = nn.Sequential(
             nn.LayerNorm(num_sim_params),
             nn.Linear(num_sim_params, embed_dim * 2),
@@ -21,9 +26,10 @@ class AdaptiveLayerNorm(nn.Module):
                 "Batch size of x and sim_params must match "
                 f"({x.shape[0]} vs {sim_params.shape[0]})"
             )
+        assert sim_params.dtype == torch.float32
 
         x = self.norm(x)
-        gb = self.modulation(sim_params)
+        gb = self.modulation(sim_params).to(self.dtype)
         gamma, beta = gb.chunk(2, dim=-1)
         shape = [sim_params.shape[0]] + [1] * (x.ndim - 2) + [self.embed_dim]
         gamma = gamma.view(shape)

src/nucleus/layers/moe/topk_moe.py
@@ -240,7 +240,7 @@ class TopkMoEOutput:
 
 # grouped_mm is cannot be cuda-graphed due to a host-device transfer, so cannot use "reduced-overhead"
 # (The offsets `offs` are moved to the CPU, and then moved back to the device when iterating over each gemm.)
-@torch.compile(fullgraph=True)
+#@torch.compile(fullgraph=True)
 class TopkMoE(nn.Module):
     def __init__(
         self,
@@ -281,7 +281,8 @@ class TopkMoE(nn.Module):
         x = x.view(-1, input_shape[-1])
         batch_size = x.shape[0]
         
-        router_output = self.router(x)
+        # Router is always applied in float32 to help stability.
+        router_output = self.router(x.to(torch.float32))
         
         # NOTE with torch.compile(fullgraph=True), the grouped gemm kernel does not support torch.float32, 
         # so the input data has to be truncated to bfloat 16.

src/nucleus/models/nucleus2_moe.py
@@ -72,8 +72,8 @@ class TransformerMoEBlock(nn.Module):
 
         self.drop_path = DropPath(drop_path_prob)
 
-        self.attention_norm = AdaptiveLayerNorm(config.embed_dim, num_sim_params)
-        self.mlp_norm = AdaptiveLayerNorm(config.embed_dim, num_sim_params)
+        self.attention_norm = AdaptiveLayerNorm(config.embed_dim, num_sim_params, dtype=config.attention_dtype)
+        self.mlp_norm = AdaptiveLayerNorm(config.embed_dim, num_sim_params, config.attention_dtype)
 
         self.router = TopkRouterWithBias(
             config.num_experts,
@@ -133,6 +133,7 @@ class MoEBase(nn.Module):
     def __init__(self, config: Nucleus2MoEConfig):
         super().__init__()
         self.config = config
+        self.embed_dtype = config.embed_dtype
         n_fields = len(self.expected_fields)
 
         self.embed = LinearEmbed(
@@ -159,7 +160,7 @@ class MoEBase(nn.Module):
             for idx in range(config.processor_blocks)
         ])
 
-        self.out_norm = nn.RMSNorm(config.embed_dim)
+        self.out_norm = nn.RMSNorm(config.embed_dim, dtype=config.debed_dtype)
         self.debed = LinearDebed(
             patch_size=config.patch_size,
             embed_dim=config.embed_dim,
@@ -186,7 +187,7 @@ class MoEBase(nn.Module):
         assert sim_params.dtype == torch.float32
 
         with record_function("encode"):
-            x = embed = self.embed(input)
+            x = embed = self.embed(input.to(self.config.embed_dtype))
 
         with record_function("get_axial_freqs"):
             with torch.no_grad():
@@ -202,6 +203,7 @@ class MoEBase(nn.Module):
         x = x + embed
 
         with record_function("debed"):
+            x = x.to(self.config.debed_dtype)
             x = self.out_norm(x)
             x = self.debed(x)
 