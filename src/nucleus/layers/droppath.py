576b26c4902d3299b689d7d87c0c35ce4c934627
Arthur Feeney <afeeney@uci.edu>
2026-06-20 21:34:24 -0700

    fix recompilation in DropPath

--- src/nucleus/layers/droppath.py
+++ src/nucleus/layers/droppath.py
@@ -1,21 +1,17 @@
 import torch
 import torch.nn as nn
-import random
-from typing import Callable
 
 class DropPath(nn.Module):
     r"""based on stochastic depth implementation from pytorch vision"""
     def __init__(self, drop_prob: float):
         super().__init__()
         self.drop_prob = drop_prob
+        # _is_identity and registering buffer prevent recompilation if using torch.compile
+        self._is_identity = drop_prob == 0.0
+        self.register_buffer("survival_rate", torch.tensor(1.0 - drop_prob))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        if not self.training or self.drop_prob == 0.0:
+        if not self.training or self._is_identity:
             return x
-        survival_rate = 1.0 - self.drop_prob
-        size = [x.shape[0]] + [1] * (x.ndim - 1)
-        noise = torch.empty(size, dtype=x.dtype, device=x.device)
-        noise = noise.bernoulli_(survival_rate)
-        if survival_rate > 0.0:
-            noise.div_(survival_rate)
+        noise = x.new_empty((x.shape[0],) + (1,) * (x.ndim - 1)).bernoulli_(self.survival_rate).div_(self.survival_rate)
         return x * noise
