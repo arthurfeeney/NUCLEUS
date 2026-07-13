55bd42cebc3d36a90f334eb9b59421fea3ab5042
Arthur Feeney <afeeney@uci.edu>
2026-07-13 10:43:22 -0700

    complete script to analyze error from downsampling

--- scripts/downsample_error.py
+++ scripts/downsample_error.py
@@ -12,10 +12,36 @@ def load_trajectory(path):
     with h5py.File(path, "r") as handle:
         return np.stack([handle[field] for field in FIELDS], axis=0)
 
-
 def vapor_volume(sdf, dx, dy):
     vapor_mask = sdf > 0
     return (vapor_mask.to(torch.float32).sum(axis=(-2, -1)) * dx * dy).mean()
+
+def temp_above_heater(temp, x_grid, dx, dy):
+    on_heater = ((x_grid >= -5.25) & (x_grid <= 5.25)).to(temp.device)
+    num_on_heater = on_heater.sum()
+    return ((temp[:, 0] * on_heater).sum(dim=-1) / num_on_heater)
+
+def heat_flux(temp, sdf, heater_temp, dx, dy):
+    def denormalize_temp_grad(temp, t_wall, t_bulk=50, k=0.054):
+        del_t = t_wall - t_bulk
+        return 2 * k * del_t * (1 - temp)
+    
+    def non_dim_temp(temp, bulk_temp=50):
+        return (temp - bulk_temp) / (heater_temp - bulk_temp)
+
+    lc = 0.0007
+    x_grid = torch.arange(-8, 8, dx) + dx / 2
+
+    liquid_mask = sdf < 0 #temp < 58
+
+    temp = non_dim_temp(temp)
+    d_temp = denormalize_temp_grad(temp[:, 0], heater_temp)
+    heater_mask = (x_grid >= -5.25) & (x_grid <= 5.25)
+    hflux_list = torch.mean((heater_mask[None, :] & liquid_mask[:, 0]).to(float) * d_temp / (dy * lc),
+                            dim=1)
+    hflux = torch.mean(hflux_list)
+    qmax = torch.max(hflux_list)
+    return hflux, qmax
 
 def main():
     parser = argparse.ArgumentParser()
@@ -27,15 +53,17 @@ def main():
     traj = torch.from_numpy(traj)
     
     print(traj.shape)
+        
+    down = traj[:, :, 0::4, ::8] # downsample starting from row above heater.
     
-    down = torch.nn.functional.interpolate(traj, (64, 64), mode="bicubic")
+    # Technically for upsampling it's more important to have the boundary,
+    # since here it's basically interpolating with ghost cells.
     traj_recon = torch.nn.functional.interpolate(down, (512, 512), mode="bicubic")
 
-    print(vapor_volume(traj[0], 1/32, 1/32))
-    print(vapor_volume(down[0], 1/4, 1/4))
-    print(vapor_volume(traj_recon[0], 1/32, 1/32))
+    print(heat_flux(traj[1], traj[0], 97, 1/32, 1/32))
+    print(heat_flux(down[1], down[0], 97, 1/4, 1/32)) # Use dy=1/32 since gap to heater unchanged
+    print(heat_flux(traj_recon[1], traj_recon[0], 97, 1/32, 1/32))
     
-
 
 if __name__ == "__main__":
     main()
